38d06c4ad98713aa3554ccd225510b13145a9cfe
bolivarhaminson <bolivarhaminson@gmail.com>
2025-10-31 08:51:01 -0500

    Update ejercicios 1listas.py

--- ejercicios 1listas.py
+++ ejercicios 1listas.py
@@ -1,1 +1,20 @@
+# Lista de frutas
 
+frutas = ["manzana", "banano", "pera", "uva", "naranja"]
+
+#primer elemento y ultimo
+print("primer elemento:", frutas[0],"\nultimo elemento:", frutas[4] )
+
+# Agregar elemento
+frutas.append("mango")
+print(frutas)
+
+# Eliminar 
+frutas.remove("pera")
+print(frutas)
+
+# Lista final
+print("lista final:",frutas)
+
+# Haminson Bolivar
+# Crhistias Barros
