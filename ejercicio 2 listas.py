86acbb563809e910905ad5a383d91eb0c1c33e0e
bolivarhaminson <bolivarhaminson@gmail.com>
2025-10-31 08:53:44 -0500

    Update ejercicio 2 listas.py

--- ejercicio 2 listas.py
+++ ejercicio 2 listas.py
@@ -1,1 +1,25 @@
+#Crea una lista llamada numeros con los numeros del 1 al 10
+ 
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 
+ 
+ 
+suma_total = sum(numeros)
+ 
+print("suma total de los numeros:", suma_total)
+ 
+ 
+ 
+numero_mas_grande = max(numeros)
+numero_mas_pequeño = min(numeros)
+ 
+print("numero mas grande:", numero_mas_grande)
+print("numero mas pequeño:", numero_mas_pequeño)
+ 
+ 
+promedio = sum(numeros) / len(numeros)
+ 
+print("promedio de la lista:", promedio)
 
+# Haminson Bolivar
+# Christian Barros
