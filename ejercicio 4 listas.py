9c3dd61855029c292c2028aaf0d69464049acc78
bolivarhaminson <bolivarhaminson@gmail.com>
2025-10-31 08:51:31 -0500

    Update ejercicio 4 listas.py

--- ejercicio 4 listas.py
+++ ejercicio 4 listas.py
@@ -1,1 +1,16 @@
+# Lista edades
+edades = [12, 17, 18, 20, 15, 22, 13]
 
+# lista mayores de 18
+mayores = []
+for edad in edades:
+    if edad >= 18:
+        mayores.append(edad)
+print("Personas de 18 años o mas:", mayores)
+
+# cantidad de mayores de 18
+numeros_mayores = len(mayores)
+print("Presonas moyores de 18:", numeros_mayores)
+
+# Haminson Bolivar
+# Crhistian Barros
