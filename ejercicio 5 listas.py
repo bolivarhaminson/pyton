a807fe5c1247a7afd7c26f7f12379b71c220e789
bolivarhaminson <bolivarhaminson@gmail.com>
2025-10-31 08:55:02 -0500

    Update ejercicio 5 listas.py

--- ejercicio 5 listas.py
+++ ejercicio 5 listas.py
@@ -1,1 +1,19 @@
+#Crea una lista con 5 notas de estudiantes
+notas = [2.2, 4.5, 5, 3.1, 4.3]
+ 
+promedio = sum(notas) / len(notas)
+print("promedio de las notas:", promedio)
+ 
+ 
+ 
+if promedio >= 3.0:
+    print("aprobado")
+else:
+    print("reprobado")
+ 
+ 
+notas_ordenadas = sorted(notas)
+print("notas ordenadas:", notas_ordenadas)
 
+# Haminson Bolivar
+# Christian Barros
