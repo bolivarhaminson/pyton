ddbaf277a1f6ceedd1a5299e2d2577e213ab45fa
bolivarhaminson <bolivarhaminson@gmail.com>
2025-10-31 08:54:26 -0500

    Update ejercicio 3 listas.py

--- ejercicio 3 listas.py
+++ ejercicio 3 listas.py
@@ -1,1 +1,19 @@
+#Dada la lista de nombres
+ 
+nombres = ["Ana", "Luis", "Sofia", "Pedro", "Carla"]
+ 
+ 
+ 
+for nombre in nombres:
+    print(nombre)
+ 
+ 
+ 
+for nombre in nombres:
+    if len(nombre) > 4:
+        print(nombre, "tiene mas de 4 letras")
+    else:
+        print(nombre, "tiene 4 letras o menos")
 
+# Haminson Bolivar
+# Christian Barros
